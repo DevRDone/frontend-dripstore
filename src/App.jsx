a1458b4ef4367ba917faa8a9dc622bc7506820ce
Cristiele <122491782+svhellen@users.noreply.github.com>
2024-08-29 18:41:42 -0300

    feat: add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Button from "./Components/Buttons";
+import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import './global.css'
 function App() {
   return (
     <>
       <Header/>
+      <Footer />
     </>
   );
 }
