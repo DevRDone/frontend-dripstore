a6117b5c6baa843fd76293c0cb0b1f34d51abf20
Ronald <devrdonee@gmail.com>
2024-09-17 00:24:10 -0300

    feature: creating collection offert component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,20 @@
 import Carousel from "../Components/Carrosel/Carrosel";
 import CollectionCategory from "../Components/CollectionCategory/CollectionCategory";
+import CollectionOffert  from "../Components/CollectionOffert/CollectionOffert";
 import FeaturedCollection from "../Components/FeaturedCollection/FeaturedCollection";
 import TrendingProducts from "../Components/TrendingProducts";
 import Layout from "./Layout";
 
 
 function HomePage() {
-    return(
+    return (
         <Layout>
             <Carousel />
-            <FeaturedCollection/>
+            <FeaturedCollection />
             <CollectionCategory />
             <TrendingProducts />
-        </Layout>            
+            <CollectionOffert />
+        </Layout>
     )
 }
 export default HomePage;
